486bb6a5b2a88bace686012b1aac9657f6a6b8c0
Its-jin0124 <itsakaguchi1125@gmail.com>
2026-05-21 14:21:35 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
-// ★ あなたの GAS WebアプリURL を入れてください
-const GAS_URL = "https://script.google.com/macros/s/AKfycbzvrbHFxRM-hL9iIbObv5NACZ4HGVwj2qR4sD81wlPi1uQAXPhc_MnNYw2hVPEk0YsG/exec";
+const GAS_URL = "https://script.google.com/macros/s/AKfycbxueBZJHpSZ0yP2YHYk9GqHX_UvPUtci--b_qVhjujGT64XIAAUgweZ0utYgUlIKKly/exec";
 
 // ▼ 表示処理（GET）→ JSONP
 fetch(`${GAS_URL}?callback=cb`)
   .then(res => res.text())
   .then(text => {
-    // JSONP → JSON に変換
     const json = text.replace(/^cb\(|\)$/g, "");
     const data = JSON.parse(json);
 
-    // F1 の日付
     document.getElementById("dateText").textContent = data.date;
 
-    // B4〜H16 の表データ
     const tbody = document.getElementById("sheetBody");
-
     data.table.forEach((row, rIndex) => {
       const tr = document.createElement("tr");
-
       row.forEach((cell, cIndex) => {
         const td = document.createElement("td");
-
         if (rIndex >= 1 && rIndex <= 12 && cIndex >= 1 && cIndex <= 4) {
           const input = document.createElement("input");
           input.value = cell;
-
           input.dataset.row = rIndex + 4;
           input.dataset.col = cIndex + 2;
-
           td.appendChild(input);
         } else {
           td.textContent = cell;
         }
-
         tr.appendChild(td);
       });
-
       tbody.appendChild(tr);
     });
   });
